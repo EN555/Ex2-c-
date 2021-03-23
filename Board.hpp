3443d2d759f1e6be68239b9ee47feb0efc8ff0a1
root <root@LAPTOP-TIC2VA1F.localdomain>
2021-03-24 00:55:11 +0200

    little changes

--- Board.hpp
+++ Board.hpp
@@ -15,7 +15,7 @@ namespace ariel{
 class Board{
     public:
         Board();
-        void post(unsigned int row, unsigned int column, Direction direction, std:: string post);
+        void post(unsigned int row, unsigned int column, Direction direction,std::string const& post);
         std:: string read(unsigned int row,unsigned int column,Direction direction, unsigned int len);
         void  show();
     private:
